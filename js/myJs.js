f195117520769b1841b577c45b7b829593f9d2c1
NguyenTruongThanh-74 <55273693+NguyenTruongThanh-74@users.noreply.github.com>
2022-06-27 14:30:07 +0700

    Update myJs.js

--- js/myJs.js
+++ js/myJs.js
@@ -1,16 +1,16 @@
 const textConfig = {
-  text1: "He luu cậu!",
-  text2: "Tớ có điều này muốn hỏi cậu nhớ phải trả lời thật lòng nhaaa.",
-  text3: "Cậu yêu tớ có phải không nào ._.",
-  text4: "Nếu cậu ko trả lời mà thoát ra tức là muốn làm vợ tớ rùi đó nha :v",
+  text1: "Chào cậu!",
+  text2: "Tớ muốn làm quen vs cậu có đồng ý ko nào",
+  text3: "Chào cậu nhá, cho tớ cơ hội làm quen nhá ._.",
+  text4: "Nếu cậu ko trả lời mà thoát ra tức là cậu đồng ý :v",
   text5: "Cậu mơ à???",
-  text6: "Yêu ơi là yêu <3",
-  text7: "lí do cậu thích tớ đi :vvvv",
+  text6: "Có có tớ đồng ý <3",
+  text7: "lí do cậu đồng ý :vvvv",
   text8: "Gửi cho tớ <3",
   text9: "Vì cậu đẹp try vlllll",
-  text10: "Tớ biết mà ^^ Yêu cậu 300.000",
+  text10: "Tớ biết mà ^^",
   text11:
-    "Tối nay tớ qua đón cậu đi chơi nhaa :v Còn giờ thì chờ gì nữa mà ko inbox cho tớ đi nàooo",
+    "Mong có cơ hội cùng cậu học tiếng anh nhá!",
   text12: "Okii lunn <3",
 };
 
